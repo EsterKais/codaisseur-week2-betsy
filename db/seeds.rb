5c07e9fa931bd6cf3457fd850aa9bf1f60b5d7b7
EsterKais <kaisester91@gmail.com>
2016-10-12 21:51:05 +0200

    Add Room Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 User.delete_all
 
-User.create(email: "laura@betsy.com", password: "secret")
-User.create(email: "josha@betsy.com", password: "secret")
-User.create(email: "jeff@betsy.com", password: "secret")
-User.create(email: "ester@betsy.com", password: "secret")
+laura = User.create(email: "laura@betsy.com", password: "secret")
+josha = User.create(email: "josha@betsy.com", password: "secret")
+jeff = User.create(email: "jeff@betsy.com", password: "secret")
+ester = User.create(email: "ester@betsy.com", password: "secret")
+
+betsy = Product.create(name: "Betsy", amount: 1, race: "Texas Longhorn", description: "This is THE Betsy!", price: 200, color: "Brown", age: 5, gender: "female", edible: false, country_origin: "The Netherlands", user: laura)
