000b2153eb57f1071521b3be6fa5673e42adba58
EsterKais <kaisester91@gmail.com>
2016-10-13 16:24:58 +0200

    Photo Integration Working

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012184525) do
+ActiveRecord::Schema.define(version: 20161013135741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_photos_on_product_id", using: :btree
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +47,13 @@ ActiveRecord::Schema.define(version: 20161012184525) do
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
 
+  create_table "products_categories", id: false, force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_categories_on_category_id", using: :btree
+    t.index ["product_id"], name: "index_products_categories_on_product_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -50,5 +71,6 @@ ActiveRecord::Schema.define(version: 20161012184525) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "photos", "products"
   add_foreign_key "products", "users"
 end
