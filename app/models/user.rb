cf5620e483db920218e204fe36458f0ea95a8da4
lauradelar <lrdela.rie@gmail.com>
2016-10-13 16:21:28 +0200

    Add User Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_one :profile
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ def has_profile?
+   profile.present?
+ end
+
+ def full_name
+   profile.full_name
+ end
+
+ def bio
+    profile.bio
+ end
+
+
 end
