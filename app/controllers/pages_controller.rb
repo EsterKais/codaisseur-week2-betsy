0add300ad1ae53c51c694dea1097cc1abce0074a
lauradelar <lrdela.rie@gmail.com>
2016-10-14 16:25:25 +0200

    tiny changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
     @products = Product.all
-
+    # @photo = Photo.find(params[:id])
+    # @product = @photo.product
+    # @product = Product.find(params[:id])
+    @photos = Photo.all
   end
-
-
-
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,9 @@ class Photo < ApplicationRecord
   belongs_to :product
 
   mount_uploader :image, ImageUploader
+
+
+  def pick_up
+    @photos = Photo.all
+  end
 end
