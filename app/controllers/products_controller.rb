b52b94a38cebd95db3c489e2c02b286cff67fdb7
EsterKais <kaisester91@gmail.com>
2016-10-18 15:01:07 +0200

    Product Controller Fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
 # this shows only products that belong to the user so we need to think about the homepage product showing
     def index
       @products = current_user.products
-      @product = Product.search(params[:search])
+      # @product = Product.search(params[:search])
     end
 
     def show
