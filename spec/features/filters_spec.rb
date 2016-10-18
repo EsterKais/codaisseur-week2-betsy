b3e8f3b58a70cca332f28d5382e09d919ce84ad0
lauradelar <lrdela.rie@gmail.com>
2016-10-18 16:22:32 +0200

    Feature test: filter edible

--- spec/features/filters_spec.rb
+++ spec/features/filters_spec.rb
@@ -16,15 +16,37 @@ describe "affordable" do
 
     expect(page).to have_text(product2.name)
     expect(page).to have_text(product3.name)
-
   end
 
   it "doesn't show products that cost more than 400 euro" do
-
     visit affordable_pages_path
 
     expect(page).not_to have_text(product1.name)
-
   end
+
+end
+
+
+describe "edible" do
+
+  let(:user) { create :user }
+
+  let!(:product1) { create :product, name: "Dude1", edible: true, user: user }
+  let!(:product2) { create :product, name: "Dude2", edible: false, user: user }
+
+
+  it "shows all products that where edible has the value true" do
+    visit edible_pages_path
+
+    expect(page).to have_text(product1.name)
+  end
+
+  it "doesn't show products where edible has the value false" do
+    visit edible_pages_path
+
+    expect(page).not_to have_text(product2.name)
+  end
+
+
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,31 +16,5 @@ RSpec.describe Product, type: :model do
   end
 
 
-# __________SCOPES____start____only tested by their behaviour? see feature tests
-  # describe ":affordable" do
-  #
-  #   let(:user) { create :user }
-  #
-  #   let!(:product1) { create :product, price: 450, user: user }
-  #   let!(:product2) { create :product, price: 200, user: user }
-  #   let!(:product3) { create :product, price: 400, user: user }
-  #
-  #
-  #   it "shows all products that cost less than 400 euro" do
-  #     expect(product1.affordable).to eq(false)
-  #     expect(product2.affordable).to eq(true)
-  #     expect(product3.affordable).to eq(true)
-  #   end
-  #
-  #   it "doesn't show products that cost more than 400 euro" do
-  #     expect(product1.affordable).not_to eq(true)
-  #     expect(product2.affordable).not_to eq(false)
-  #     expect(product3.affordable).not_to eq(false)
-  #   end
-  #
-  # end
-
-# __________FILTERS____end____
-
-
+#  Scopes, like validations, are only tested by their behaviour. see feature test
 end
