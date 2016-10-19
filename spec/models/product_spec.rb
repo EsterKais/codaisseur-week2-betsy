227c4880f810efd19cdd885c3bbbaa25905b693f
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-10-19 19:02:15 +0200

    Add profile test validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
 
-  describe "association with order" do
-    let(:buyer_user) { create :user, email: "guest@user.com" }
-    let(:host_user) { create :user, email: "host@user.com" }
-
-    let!(:product) { create :product, user: host_user }
-    let!(:order) { create :order, product: product, user: buyer_user }
-
-    it "has buyers" do
-      expect(product.buyers).to include(buyer_user)
-    end
-
-    it "deletes associated order" do
-      expect { product.destroy }.to change(Order, :count).by(-1)
-    end
-  end
+  # describe "association with order" do
+  #   let(:buyer_user) { create :user, email: "guest@user.com" }
+  #   let(:host_user) { create :user, email: "host@user.com" }
+  #
+  #   let!(:product) { create :product, user: host_user }
+  #   let!(:order) { create :order, product: product, user: buyer_user }
+  #
+  #   it "has buyers" do
+  #     expect(product.buyers).to include(buyer_user)
+  #   end
+  #
+  #   it "deletes associated order" do
+  #     expect { product.destroy }.to change(Order, :count).by(-1)
+  #   end
+  # end
 
   describe "association with user" do
     let(:user) {create :user }
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#  pending "add some examples to (or delete) #{__FILE__}"
+#--- validation testing ---
+  describe "validations" do
+    it "is invalid without a first name" do
+      profile = Profile.new(first_name: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:first_name)
+    end
+
+    it "is invalid without a last name" do
+      profile = Profile.new(last_name: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:first_name)
+    end
+
+    it "is invalid without a bio" do
+      profile = Profile.new(bio: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:bio)
+    end
+  end
+#--- end of validation testing ---
+
 end
