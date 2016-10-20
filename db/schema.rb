fe15e735443e7f572cb4e590c03b2b35772e65bd
lauradelar <lrdela.rie@gmail.com>
2016-10-20 07:51:34 +0200

    migrations and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014105958) do
+ActiveRecord::Schema.define(version: 20161020054613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.integer  "quantity"
+    t.decimal  "item_price"
+    t.decimal  "line_total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_line_items_on_order_id", using: :btree
+    t.index ["product_id"], name: "index_line_items_on_product_id", using: :btree
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "order_number"
+    t.decimal  "total_price"
+    t.string   "payment"
+    t.string   "shipping"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
+  end
 
   create_table "photos", force: :cascade do |t|
     t.integer  "product_id"
@@ -68,7 +91,8 @@ ActiveRecord::Schema.define(version: 20161014105958) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "orders", "products"
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "photos", "products"
   add_foreign_key "products", "users"
